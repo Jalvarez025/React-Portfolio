850e547a1a7442e43f618d934dfc01888b42ac5d
Jalvarez025 <90490928+Jalvarez025@users.noreply.github.com>
2021-12-07 21:11:36 -0600

    Recreated portfolio added Home page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,27 @@
+import logo from './logo.svg';
 import './App.css';
-import Portfolio from './components/portfolio';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
-    <div className="portfolio-app">
-      <Portfolio />
-    </div>
-    
-  )
+    <Router>
+      <NavTabs />
+      
+      <Switch>
+        <Route exact path={["/", "/JesseAlvarez"]} component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+      </Switch>
+
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
