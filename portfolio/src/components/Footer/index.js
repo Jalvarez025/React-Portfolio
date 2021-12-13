f424f59e55db05b1e4c0642920bc3e0518438ee7
Jalvarez025 <90490928+Jalvarez025@users.noreply.github.com>
2021-12-13 17:52:28 -0600

    M

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -1,30 +1,11 @@
 import React from 'react';
 
 function Footer() {
-
-  // Replace links with social media profiles
-  const icons = [
-    {
-      name: "fab fa-github",
-      link: "https://github.com/"
-    },
-    {
-      name: "fab fa-linkedin",
-      link: "https://www.linkedin.com/"
-    },
-    {
-      name: "fab fa-stack-overflow",
-      link: "https://stackoverflow.com/"
-    }
-  ]
-
   return (
-    <footer className="flex-row px-1">
-      {icons.map(icon =>
-      (
-        <a href={icon.link} key={icon.name} target="_blank" rel="noopener noreferrer"><i className={icon.name}></i></a>
-      )
-        )}
+    <footer className="flex-row px-1">  
+    <a href="https://github.com/Jalvarez025" target="_blank" rel="noopener noreferrer">Github</a>
+    <a href="https://www.linkedin.com/in/jesse-alvarez-a73092129/" target="_blank" rel="noopener noreferrer">Linkedin</a>
+      
     </footer>
   );
 }
